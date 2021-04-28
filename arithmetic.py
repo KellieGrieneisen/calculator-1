9bc818e0132ad3531d7d99a3c7251fe8ea2400cc
KellieGrieneisen <kellie_grieneisen@aol.com>
2021-04-28 19:33:16 +0000

    revise square, cube, power and mod functions
    Co-authored by: Maura Thompson <thomaura@gmail.com>

--- arithmetic.py
+++ arithmetic.py
@@ -26,15 +26,23 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of the input."""
+    answer = "%.1f" %(num1**2)
+    return answer
 
 
 def cube(num1):
     """Return the cube of the input."""
+    answer = "%.1f" %(num1**3)
+    return answer
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    answer = "%.1f" %(num1**num2)
+    return answer
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    answer = "%.1f" %(num1%num2)
+    return answer
